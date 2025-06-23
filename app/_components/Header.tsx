11f1d738a22e3fa2fbd554e9045882a1bb6fdfaa
Tambi <tambi@takingroot.org>
2025-06-23 09:59:34 -0700

    Update Header.tsx
    change subtitle and add logo

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 const Header: React.FC = () => {
   return (
@@ -6,11 +7,20 @@ const Header: React.FC = () => {
       <div className="container mx-auto px-4 py-2">
         <div className="">
           <div className="flex items-center justify-between md:justify-start md:space-x-2">
+             <Image
+              src="https://rented123-brand-files.s3.us-west-2.amazonaws.com/logo_white.svg"
+              alt="logo"
+              width="80"
+              height={80}
+              className="w-15 h-15 md:w-20 md:h-20"
+            />
             <div>
               <h1 className="text-lg md:text-2xl font-bold text-[#293074]">
                   AI Intelligent Background Verification
               </h1>
-        
+             <p className="text-sm text-[#293074]">
+                Save money and time by getting instant background verification of your prospect
+              </p>
             </div>
           </div>
         </div>
